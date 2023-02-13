0d64db8c2c74f21ee6a62f9ce83c54f7c14e2f6a
André Craco <49844995+andrecrax@users.noreply.github.com>
2023-02-12 23:45:32 -0300

    username

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,27 @@
 
-
+import React, { useState } from 'react';
 export default function Sidebar(){
+  const [urlProfile, setUrlProfile] = useState(null);
+
+  function changeProfilePhoto() {
+    const newUrl = prompt("Insira o link da imagem desejada:");
+    setUrlProfile(newUrl);
+  }
+
+  const [userName, setUserName] = useState(null);
+
+  function changeUserName() {
+    const newName = prompt("Insira o nome que deseja:");
+    setUserName(newName);
+  }
+
     return (      <div className="sidebar">
     <div className="usuario">
-      <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+      <img data-test="profile-image" onClick={() => changeProfilePhoto()} src={urlProfile || "assets/img/catanacomics.svg"} alt="imagem de perfil"/>
       <div className="texto">
-        <span>
-          <strong>catanacomics</strong>
-          <ion-icon name="pencil"></ion-icon>
+        <span data-test="name">
+          <strong>{userName || "catanacomics"}</strong>
+          <ion-icon data-test="edit-name" onClick={() => changeUserName()} name="pencil"></ion-icon>
         </span>
       </div>
     </div>
@@ -20,7 +34,7 @@ export default function Sidebar(){
 
       <div className="sugestao">
         <div className="usuario">
-          <img src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
+          <img  src="assets/img/bad.vibes.memes.svg" alt="bad.vibes.memes.svg"/>
           <div className="texto">
             <div className="nome">bad.vibes.memes</div>
             <div className="razao">Segue você</div>
